159e24550d949c19de847d959cec43d4dc2deda0
FelpsSousa <felipeluis.felpssousa@gmail.com>
2023-10-21 10:49:32 -0300

    Definindo tema da aplicação. ThemeProvider.

---
+++ portifolio/src/styles/theme.js
@@ -0,0 +1,26 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#232129",
+    BACKGROUND_800: "#312E38",
+    BACKGROUND_700: "#3E3B47",
+
+    WHITE: "#F4EDE8",
+    ORANGE: "#FF9000",
+
+    GRAY_100: "#999591",
+    GRAY_300: "#666360",
+
+    RED: "#FF002E",
+
+    COLOR_1: "#060606",
+    COLOR_2: "#1c2843",
+    COLOR_3: "#526391",
+    COLOR_4: "#879fd9",
+    COLOR_5: "#b7d6e5",
+    COLOR_6: "#000008",
+    COLOR_7: "#1c294b",
+    COLOR_8: "#4b5999",
+    COLOR_9: "#8a8ff2",
+    COLOR_10: "#d7cbff",
+  }
+}
